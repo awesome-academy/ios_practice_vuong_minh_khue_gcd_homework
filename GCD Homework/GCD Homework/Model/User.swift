455e3299e9a6110abf6a2989017e6d62abc6606e
Khuevm <vngminhkhue@gmail.com>
2023-07-26 12:42:04 +0700

    Add Music List View

---
+++ GCD Homework/GCD Homework/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  GCD Homework
+//
+//  Created by Khue on 25/07/2023.
+//
+
+import Foundation
+
+struct User {
+    let avatarURL: String
+    let name: String
+    let account: String
+}
